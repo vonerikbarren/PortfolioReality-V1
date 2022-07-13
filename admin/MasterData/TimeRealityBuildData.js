5cbcedbae97d86d5ce0f470d5db6e9b94ebcd961
Von'Erik Barren-Tucker <barrentucker.vonerik@gmail.com>
2022-07-13 10:51:09 -0400

    initalizing of files for repo

---
+++ admin/MasterData/TimeRealityBuildData.js
@@ -0,0 +1,21 @@
+const TimeΔ_Data = {
+   objectiveList: {
+      objective01: ["Edit build for expansion protocol"],
+      objective02: ["Check tRings against ipad/concepts/plans + add additional tRings where necessary"],
+      objective03: ["Label with css3D - remember small size, scale up, scene.add()"],
+      objective04: ["Build camera model for controller viewer"]
+      objective05: ["Look up time functions and how to exectue + GSAP"]
+      objective06: ["Try adding more materials and switch to standard material."]
+      objective07: ["Make a new CSS3D array after changing chemical elements to millitary times"],
+      objective08: ["Using databases keep track of time events"]
+      objective09: ["Run tests"]
+   }
+}
+
+
+
+const time_ΔOД = {
+   orbitRing01: {
+      
+   }
+}
---
+++ admin/TimeStories/00-Learning/tests/piratesOfTheCarribean.js
@@ -0,0 +1,28 @@
+const piratesOfTheCarribean = {
+   // #############################
+   // |- # Comment Seperator between scenes or mini scenes. 
+   // #############################
+   SceneNumA00: {
+
+      // Header
+      SceneHeader: {
+         SceneTitle: "",
+         SceneLocationInSpace: ["", ''], // [ Name of Location, Exterior vs Interior ] 
+         SceneLocationInTime: "",
+         SceneLocationInReality: "",
+      },
+      
+      // Scene 
+      SceneDetailsData: {
+         SceneDetails: {},
+
+         SceneDialogue: {},
+         SceneLyrics: {},
+         SceneAudio: {},
+         SceneMusic: {},
+         SceneVisuals: {},
+         SceneEffects: {},
+      },
+
+   },
+}
